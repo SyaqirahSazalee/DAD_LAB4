234d791f81d0170479205dc26ef2eb5402ab6698
Syaqirah Sazalee <syaqirahnf@gmail.com>
2021-04-17 15:17:38 +0800

    Initial Commit

---
+++ demotcpclienttext/src/demotcpclienttext/ClientTextApplication.java
@@ -0,0 +1,44 @@
+/*
+ *This program run as client side to ask request and accept number of words in the text  
+ *
+ *@author SyaqirahSazalee
+ */
+package demotcpclienttext;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class ClientTextApplication {
+
+	public static void main(String[] args) 
+			throws UnknownHostException, IOException {
+		
+		// Launch client-side frame
+		ClientTextFrame clientTextFrame = new ClientTextFrame();
+		clientTextFrame.setVisible(true);
+		
+		// Connect to the server @ localhost, port 4228
+		Socket socket = new Socket(InetAddress.getLocalHost(), 4228);
+		
+		// Update the status of the connection
+		clientTextFrame.updateConnectionStatus(socket.isConnected());
+		
+		// Read from network
+		BufferedReader bufferedReader = new BufferedReader(
+				new InputStreamReader(socket.getInputStream()));
+		
+		// Display the current text
+		String currentText = bufferedReader.readLine();
+		clientTextFrame.updateServerText(currentText);
+		
+		// Close everything
+		bufferedReader.close();
+		socket.close();
+
+	}
+
+}
